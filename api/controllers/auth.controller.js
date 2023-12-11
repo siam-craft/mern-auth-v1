8c08462556f6f8adb10cb872d6fdd7ddb42317c1
siam-craft <abdullahsiam009@gmail.com>
2023-12-11 15:02:01 +0600

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const singup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -8,6 +10,30 @@ export const singup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json({ message: "User created successfully" });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong credentials"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: _password, ...others } = validUser._doc;
+    const expirationDate = new Date(Date.now() + 60 * 60 * 1000);
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        expires: expirationDate,
+      })
+      .status(200)
+      .json(others);
   } catch (err) {
     next(err);
   }
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { singup } from "../controllers/auth.controller.js";
+import { singup, signin } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/signup", singup);
+router.post("/signin", signin);
 
 export default router;
