c1d5fcaeb33d71c269566b249cb3252c184c33fe
siam-craft <abdullahsiam009@gmail.com>
2023-12-12 15:48:20 +0600

    private route create for profile page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const { currentUser } = useSelector((state) => state.persistedReducer.user);
+  console.log(currentUser, "currentUser");
   return (
     <div className="bg-blue-500 p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white text-lg font-bold">
+        <div className="text-white text-lg font-bold ">
           <NavLink to="/" className="text-white hover:text-gray-300">
             Auth App
           </NavLink>
         </div>
-        <nav className="space-x-4">
+        <nav className="space-x-4 flex items-center justify-center">
           <NavLink to="/" className="text-white hover:text-gray-300">
             Home
           </NavLink>
@@ -20,12 +23,27 @@ const Header = () => {
           <NavLink to="/profile" className="text-white hover:text-gray-300">
             Profile
           </NavLink>
-          <NavLink to="/sign-in" className="text-white hover:text-gray-300">
-            Login
-          </NavLink>
-          <NavLink to="/sign-up" className="text-white hover:text-gray-300">
-            Register
-          </NavLink>
+          {currentUser ? (
+            <img
+              className="w-8 h-8 rounded-full object-cover"
+              src={currentUser.profilePicture}
+              alt=""
+            />
+          ) : (
+            <NavLink to="/sign-in" className="text-white hover:text-gray-300">
+              Login
+            </NavLink>
+          )}
+
+          {currentUser ? (
+            <button type="button" className="text-white hover:opacity-70">
+              Log Out
+            </button>
+          ) : (
+            <NavLink to="/sign-up" className="text-white hover:text-gray-300">
+              Register
+            </NavLink>
+          )}
         </nav>
       </div>
     </div>
