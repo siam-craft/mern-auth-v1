fde60e6957ccf045926cd1461cb837c6b5995f5c
siam-craft <abdullahsiam009@gmail.com>
2023-12-19 14:17:45 +0600

    update user api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 dotenv.config();
@@ -15,6 +16,8 @@ mongoose
 const app = express();
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
